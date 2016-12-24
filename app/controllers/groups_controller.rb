0119bf35d1980f93c8ec9ac1e6311d63f3f21463
xueshengyt <footmark@163.com>
2016-12-24 23:38:17 +0800

    implement my post

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,7 +22,6 @@ class GroupsController < ApplicationController
   def create
     @group = Group.new(group_params)
     @group.user = current_user
-
     if @group.save
       current_user.join!(@group)
       redirect_to groups_path
