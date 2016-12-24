857ba7620cefab58603494d4e2612ee5df660d1c
xueshengyt <footmark@163.com>
2016-12-24 12:16:59 +0800

    implement group relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,5 @@ class User < ApplicationRecord
   has_many :posts
 
   has_many :group_relationships
-has_many :participated_groups, :through => :group_relationships, :source => :group
-end
+ has_many :participated_groups, :through => :group_relationships, :source => :group
 end
